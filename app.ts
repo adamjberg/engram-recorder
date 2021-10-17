96561b26e33e7115740d5308b2b9f1680be070d4
Adam Berg <adam@xyzdigital.com>
2021-10-17 14:39:00 -0700

    Working multiple recordings

--- app.ts
+++ app.ts
@@ -2,23 +2,77 @@ let audioIN = { audio: true };
 
 let recording = false;
 const btnRecord = document.getElementById("btn-record");
+const clipList = document.getElementById("clip-list");
+let mediaRecorder = null;
 
 try {
-  navigator.mediaDevices.getUserMedia(audioIN).then(function (mediaStreamObj) {
-    alert("got media")
-    let mediaRecorder = new MediaRecorder(mediaStreamObj);
-
-    btnRecord.addEventListener('click', function (ev) {
-      alert("clicked")
-      btnRecord.classList.toggle("btn-record_recording")
-      if (recording) {
-        mediaRecorder.stop();
-      } else {
-        mediaRecorder.start();
-      }
+  navigator.mediaDevices
+    .getUserMedia(audioIN)
+    .then(function (mediaStreamObj) {
+      btnRecord.addEventListener("click", function (ev) {
+        btnRecord.classList.toggle("btn-record__recording");
+        if (recording) {
+          stopRecording();
+        } else {
+          mediaRecorder = startRecording(mediaStreamObj);
+        }
+        recording = !recording;
+      });
     })
-  }).catch((err) =>{ alert(err.message) })
-} catch(err) {
-  alert(err.message)
+    .catch((err) => {
+      alert(err.message);
+    });
+} catch (err) {
+  alert(err.message);
 }
 
+function startRecording(mediaStreamObj) {
+  let mediaRecorder = new MediaRecorder(mediaStreamObj);
+  let chunks = [];
+
+  mediaRecorder.onstop = function (e) {
+    let clipName = prompt("Enter a name for your sound clip");
+    if (!clipName) {
+      clipName = new Date().toLocaleDateString();
+    }
+
+    const clipContainer = document.createElement("div");
+    clipContainer.classList.add("clip");
+
+    clipContainer.addEventListener("click", function () {
+      clipContainer.classList.toggle("clip__selected");
+    });
+
+    const clipLabel = document.createElement("div");
+    clipLabel.classList.add("clip-label");
+    clipLabel.innerHTML = clipName;
+    clipContainer.appendChild(clipLabel);
+
+    const audio = document.createElement("audio");
+
+    audio.setAttribute("controls", "");
+
+    clipContainer.appendChild(audio);
+    clipList.appendChild(clipContainer);
+
+    audio.controls = true;
+    const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+    chunks = [];
+    const audioURL = URL.createObjectURL(blob);
+    audio.src = audioURL;
+  };
+
+  mediaRecorder.ondataavailable = function (e) {
+    chunks.push(e.data);
+  };
+
+  mediaRecorder.start();
+
+  return mediaRecorder;
+}
+
+function stopRecording() {
+  if (mediaRecorder) {
+    mediaRecorder.stop();
+  }
+}
