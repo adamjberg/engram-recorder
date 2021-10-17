844d42f21debafdcd68f337d52cd5ca1153ab847
Adam Berg <adam@xyzdigital.com>
2021-10-17 17:06:46 -0400

    Add media recorder

--- app.ts
+++ app.ts
@@ -1,3 +1,24 @@
-const elemDiv = document.createElement('div');
+let audioIN = { audio: true };
 
-document.body.appendChild(elemDiv);
+let recording = false;
+const btnRecord = document.getElementById("btn-record");
+
+try {
+  navigator.mediaDevices.getUserMedia(audioIN).then(function (mediaStreamObj) {
+    alert("got media")
+    let mediaRecorder = new MediaRecorder(mediaStreamObj);
+
+    btnRecord.addEventListener('click', function (ev) {
+      alert("clicked")
+      btnRecord.classList.toggle("btn-record_recording")
+      if (recording) {
+        mediaRecorder.stop();
+      } else {
+        mediaRecorder.start();
+      }
+    })
+  }).catch((err) =>{ alert(err.message) })
+} catch(err) {
+  alert(err.message)
+}
+
